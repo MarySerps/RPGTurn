330b67b906606bf4bbbb043a4785781ef7b86e59
Mariana Serpa <marianacserpa@icloud.com>
2026-03-13 15:39:17 +0000

    Ataque especial Guerreiro

--- src/Warrior.java
+++ src/Warrior.java
@@ -4,6 +4,16 @@ public class Warrior extends Player  {
         super(name, hp, strength);
     }
 
+    public specialAttack (Player target){
+        System.out.println (this.getName() + "usa o golpe especial");
+
+        int danoEspecial = this.strength() * 2;
+        target.Damage(danoEspecial);
+
+        System.out.println (this.getName() + "Gasta 5 de Hp ");
+        this.Damage(5);
+    }
+
     @Override
     public void attack(Player target) {
         System.out.println(this.getName() + " ataca dando um dano de " + this.getStrength());
